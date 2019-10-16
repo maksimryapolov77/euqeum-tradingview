9697977cbd68e7c86feb5a0eaa1c161a650fd858
maksimryapolov <maksimryapolov77@yandex.com>
2019-10-16 20:00:02 +0300

    grid column fit to screen

--- src/app/app-grid/app-grid.component.ts
+++ src/app/app-grid/app-grid.component.ts
@@ -5,9 +5,6 @@ import { AppGridService } from '../service/app-grid.service';
 import { HttpClient } from '@angular/common/http';
 import { TvChartContainerComponent } from '../tv-chart-container/tv-chart-container.component';
 
-import {
-  ChartingLibraryWidgetOptions,
-} from '../../assets/charting_library/charting_library.min';
 
 @Component({
   selector: 'app-grid',
@@ -18,16 +15,16 @@ export class AppGridComponent implements OnInit {
   @Output() symbolChanged = new EventEmitter<string>();
 
   columnDefs = [
-    { headerName: 'Symbol', field: 'v.short_name', sortable: true, filter: true },
-    { headerName: 'Original Name', field: 'v.original_name', sortable: true, filter: true },
-    { headerName: 'Lp', field: 'v.lp', sortable: true, filter: true },
-    { headerName: 'Ask', field: 'v.ask', sortable: true, filter: true },
-    { headerName: 'Bid', field: 'v.bid', sortable: true, filter: true },
-    { headerName: 'Open Price', field: 'v.open_price', sortable: true, filter: true },
-    { headerName: 'High Price', field: 'v.high_price', sortable: true, filter: true },
-    { headerName: 'Low Price', field: 'v.low_price', sortable: true, filter: true },
-    { headerName: 'Prev Close Price', field: 'v.prev_close_price', sortable: true, filter: true },
-    { headerName: 'Volume', field: 'v.volume', sortable: true, filter: true },
+    { headerName: 'Symbol', field: 'v.short_name', sortable: true, filter: true},
+    { headerName: 'Original Name', field: 'v.original_name', sortable: true, filter: true, resizable: true },
+    { headerName: 'Lp', field: 'v.lp', sortable: true, filter: true, resizable: true },
+    { headerName: 'Ask', field: 'v.ask', sortable: true, filter: true, resizable: true },
+    { headerName: 'Bid', field: 'v.bid', sortable: true, filter: true, resizable: true },
+    { headerName: 'Open Price', field: 'v.open_price', sortable: true, filter: true, resizable: true },
+    { headerName: 'High Price', field: 'v.high_price', sortable: true, filter: true, resizable: true },
+    { headerName: 'Low Price', field: 'v.low_price', sortable: true, filter: true, resizable: true },
+    { headerName: 'Prev Close Price', field: 'v.prev_close_price', sortable: true, filter: true, resizable: true },
+    { headerName: 'Volume', field: 'v.volume', sortable: true, filter: true, resizable: true },
   ];
 
   private gridApi;
